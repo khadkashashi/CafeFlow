20baac37ef7935ccc8892f4a5f38386f823929db
shashi khadka <shashikhadka84@gmail.com>
2026-08-15 23:17:50 +0545

    add login logout

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
-
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "landing:home"
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
--- core/urls.py
+++ core/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="landing:home"), name="logout"),
     path('orders/', include("orders.urls")),
     path('kitchen/',include("kitchen.urls")),
     path('reception/', include("tables.urls")),
