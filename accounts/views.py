965ae64a5ee88bd181e6d6d20f3937e075c6fc32
shashi khadka <shashikhadka84@gmail.com>
2026-08-22 23:52:04 +0545

    Add public customer signup, restricted to Customer role only

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from .forms import CustomerSignUpForm
+from .models import User
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomerSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = User.Role.CUSTOMER
+            user.save()
+            login(request, user)
+            return redirect("landing:home")
+    else:
+        form = CustomerSignUpForm()
+
+    return render(request, "accounts/signup.html", {"form": form})
--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from accounts import views as account_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,6 +35,7 @@ urlpatterns = [
     path("payments/", include("payments.urls")),
     path("", include("landing.urls")),
     path("account/", include("customers.urls")),
+    path("signup/", account_views.signup, name="signup"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
